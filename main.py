f88c4b9721e4f06c1a3e5048429cef3d250fc91a
naum <yehornaumenko@gmail.com>
2019-01-07 14:42:04 +0200

    Has login/email already used

--- main.py
+++ main.py
@@ -10,17 +10,26 @@ def sign_up():
 
     user = dict()
     user["login"] = request.values["login"]
-    user["password"] = request.values["password"]
     user["email"] = request.values["email"]
+    user["password"] = request.values["password"]
     user["status"] = 'inactive'
+
+    conn = sqlite3.connect("debt.db")
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM users WHERE login=?', (user["login"], ))
+    if cursor.fetchone() is not None:
+        return "Try another login."
+
+    cursor.execute('SELECT * FROM users WHERE email=?', (user["email"],))
+    if cursor.fetchone() is not None:
+        return "Try another email."
 
     if not functions.log_pass_valid(user["login"], user["password"]):
         return functions.invalid_data_msg()
 
     user = functions.dict_to_tuple(user)
     print(user)
-    conn = sqlite3.connect("debt.db")
-    cursor = conn.cursor()
     cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?)", user)
     conn.commit()
     conn.close()
